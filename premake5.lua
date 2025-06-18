d1284f1a3dfb82e882f11f44b920bd4084d19e16
EasySouls <tarjanyicsanad@gmail.com>
2025-06-18 22:31:23 +0200

    Jolt builds with premake

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "JoltPhysics"
+  kind "StaticLib"
+  language "C++"
+  cppdialect "C++20"
+  staticruntime "off"
+
+  targetdir("bin/" .. outputdir .. "/%{prj.name}")
+  objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+      "Jolt/**.h",
+      "Jolt/**.cpp",
+  }
+
+  includedirs
+  {
+      ".",
+  }
+
+  filter "system:windows"
+    systemversion "latest"
+
+  filter "system:linux"
+    pic "On"
+    systemversion "latest"
+
+  filter "configurations:Debug"
+    runtime "Debug"
+    symbols "on"
+
+  filter "configurations:Release"
+    runtime "Release"
+    optimize "on"
